20a1371a10dbd374e047d1b767c65b0b881f0f75
Victoria Pongsatyapipat <victoria.pong@versent.com.sg>
2021-10-27 14:39:28 +0800

    chore: add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+
+const TextHead = styled.h2`
+  font-size: 32px;
+  color: ${(props) => props.theme.color.primary};
+`;
 
 const App: React.FC = () => {
-  return <div>testing</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <TextHead>Versent</TextHead>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  color: {
+    primary: '#98CA3E',
+    secondary: '#1CA1C9',
+  },
+};
+
+export default theme;
