19d1569c81f20ee6008df04643860beb4a6b5a12
Victoria Pongsatyapipat <victoria.pong@versent.com.sg>
2021-10-28 10:30:16 +0800

    chore: add Storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,21 @@
+import GlobalStyles from '../src/styles/global-styles';
+import { css } from 'styled-components';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  ),
+];
